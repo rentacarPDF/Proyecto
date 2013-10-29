16bd052a5410641a96429dd75d909f042440d43c
Matias Prado <matiasprado@msn.com>
2013-10-29 16:40:37 -0300

    Se agregó la entidad Cliente. Además se agregó su respectivo servicio, que incluye un listado de clientes activos.

---
+++ dom/src/main/java/cliente/Cliente.java
@@ -0,0 +1,170 @@
+package cliente;
+
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.VersionStrategy;
+import org.apache.isis.applib.DomainObjectContainer;
+import org.apache.isis.applib.annotation.AutoComplete;
+import org.apache.isis.applib.annotation.DescribedAs;
+import org.apache.isis.applib.annotation.Hidden;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.Named;
+import org.apache.isis.applib.annotation.ObjectType;
+import org.apache.isis.applib.annotation.RegEx;
+import org.apache.isis.applib.filter.Filter;
+import org.apache.isis.applib.util.TitleBuffer;
+import com.google.common.base.Objects;
+
+@javax.jdo.annotations.PersistenceCapable(identityType = IdentityType.DATASTORE)
+@javax.jdo.annotations.DatastoreIdentity(strategy = javax.jdo.annotations.IdGeneratorStrategy.IDENTITY)
+@javax.jdo.annotations.Queries({ @javax.jdo.annotations.Query(name = "listado_cliente", language = "JDQL", value = "SELECT * FROM dom.cliente.Cliente WHERE activo== :true ") })
+@javax.jdo.annotations.Version(strategy = VersionStrategy.VERSION_NUMBER, column = "VERSION")
+@ObjectType("Cliente")
+@AutoComplete(repository = ClienteServicio.class, action = "autoComplete")
+
+public class Cliente {
+
+	public static enum TipoId {
+		CUIL, CUIT;
+	}	
+	
+	@Named("Cliente")
+	// {{ Identification on the UI
+	public String title() {
+		final TitleBuffer buf = new TitleBuffer();
+		buf.append(getNumeroIdent());
+		return buf.toString();
+	}
+	// }}
+
+	// {{ OwnedBy (property)
+	private String ownedBy;
+
+	@Hidden
+	public String getOwnedBy() {
+		return ownedBy;
+	}
+	public void setOwnedBy(final String ownedBy) {
+		this.ownedBy = ownedBy;
+	}
+	// }}
+
+	// {{ Nombre
+	private String nombre;
+
+	@RegEx(validation = "\\w[@&:\\-\\,\\.\\+ \\w]*")
+	@MemberOrder(sequence = "1")
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	// }}
+
+	// {{ Apellido
+	private String apellido;
+
+	@RegEx(validation = "\\w[@&:\\-\\,\\.\\+ \\w]*")
+	@MemberOrder(sequence = "2")
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	// }}
+
+	// {{ Tipo de Identificacion Tributaria
+	private TipoId tipo;
+
+	@DescribedAs("Señala el tipo de documento")
+	@RegEx(validation = "\\w[@&:\\-\\,\\.\\+ \\w]*")
+	@MemberOrder(sequence = "3")
+	public TipoId getTipoId() {
+		return tipo;
+	}
+	public void setTipoId(TipoId tipo) {
+		this.tipo = tipo;
+	}
+	// }}
+
+	// {{ Numero de Identificacion Tributaria
+	private String numeroIdent;
+	@RegEx(validation = "\\w[@&:\\-\\,\\.\\+ \\w]*")
+	@MemberOrder(sequence = "4")
+	public String getNumeroIdent() {
+		return numeroIdent;
+	}
+	public void setNumeroIdent(String numeroId) {
+		this.numeroIdent = numeroId;
+	}
+	// }}
+	
+	// {{ Numero de telefono
+	private int numeroTel;
+
+	@RegEx(validation = "\\w[@&:\\-\\,\\.\\+ \\w]*")
+	@MemberOrder(sequence = "5")
+	public int getNumeroTel() {
+		return numeroTel;
+	}
+	public void setNumeroTel(int numeroTel) {
+		this.numeroTel = numeroTel;
+	}
+	// }}	
+	
+	// {{ Correo electronico
+	private String mail;
+
+	@RegEx(validation = "\\w[@&:\\-\\,\\.\\+ \\w]*")
+	@MemberOrder(sequence = "6")
+	public String getEmail() {
+		return mail;
+	}
+	public void setEmail(String mail) {
+		this.mail = mail;
+	}
+	// }}	
+	
+		
+	// {{ Activo
+	private boolean activo;
+
+	@Hidden
+	@MemberOrder(sequence = "9")
+	public boolean getActivo() {
+		return activo;
+	}
+	public void setActivo(boolean activo) {
+		this.activo = activo;
+	}
+	// }}
+
+	// {{ Remove (action)
+	public void remove() {
+		setActivo(false);
+	}
+	// }}
+
+	// {{ Filtro
+	public static Filter<Cliente> thoseOwnedBy(final String currentUser) {
+		return new Filter<Cliente>() {
+			@Override
+			public boolean accept(final Cliente cliente) {
+				return Objects.equal(cliente.getOwnedBy(), currentUser);
+			}
+		};
+	}
+	// }}
+
+	// {{ injected: DomainObjectContainer
+	private DomainObjectContainer container;
+
+	protected DomainObjectContainer getContainer() {
+		return container;
+	}
+	public void setDomainObjectContainer(final DomainObjectContainer container) {
+		this.container = container;
+	}
+	// }}
+}
---
+++ dom/src/main/java/cliente/ClienteServicio.java
@@ -0,0 +1,118 @@
+package cliente;
+
+import java.util.List;
+import org.apache.isis.applib.AbstractFactoryAndRepository;
+import org.apache.isis.applib.annotation.ActionSemantics;
+import org.apache.isis.applib.annotation.ActionSemantics.Of;
+import org.apache.isis.applib.annotation.Hidden;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.Named;
+import org.apache.isis.applib.filter.Filter;
+import com.google.common.base.Objects;
+
+import cliente.Cliente.TipoId;
+
+
+
+@Named("Cliente")
+public class ClienteServicio extends AbstractFactoryAndRepository {
+	
+	// {{ Carga de clientes
+	@MemberOrder(sequence = "1")
+	public Cliente CargarCliente(@Named("Nombre") String nombre,
+			@Named("Apellido") String apellido,
+			@Named("Tipo de Id Tributaria") TipoId tipo,
+			@Named("Numero") String numeroId,
+			@Named("Numero de Telefono") int numeroTel,
+			@Named("Correo Electrónico") String mail){
+		final String ownedBy = currentUserName();
+		final boolean activo = true;
+		return elCliente(nombre, apellido, tipo, numeroId, numeroTel, mail, ownedBy, activo);
+	}	
+	@Hidden
+	public Cliente elCliente(
+			final String nombre, 
+			final String apellido, 
+			final TipoId tipo,
+			final String numeroId, 
+			final int numeroTel, 
+			final String mail,
+			final String userName, 
+			final boolean activo) {		
+			final List<Cliente> mismoNumDoc = allMatches(Cliente.class,
+				new Filter<Cliente>() {
+					@Override
+					public boolean accept(final Cliente cliente) {
+						return Objects.equal(cliente.getNumeroIdent(), numeroId);
+					}
+				});
+			
+
+		Cliente cliente = newTransientInstance(Cliente.class);
+
+		if (mismoNumDoc.size() == 0) {
+
+			cliente.setNombre(nombre);
+			cliente.setApellido(apellido);
+			cliente.setTipoId(tipo);
+			cliente.setNumeroIdent(numeroId);
+			cliente.setNumeroTel(numeroTel);
+			cliente.setEmail(mail);
+			cliente.setOwnedBy(userName);
+			cliente.setActivo(true);
+			
+			
+			persistIfNotAlready(cliente);			
+			
+		} else {
+			cliente = null;
+			getContainer().warnUser("YA SE ENCUENTRA EL CLIENTE CARGADO");
+			System.out.println("YA SE ENCUENTRA EL CLIENTE CARGADO");
+			
+		}
+
+		return cliente;
+	} 
+	// }}
+	
+	// {{ Listado de Clientes Activos
+	@ActionSemantics(Of.SAFE)
+	@MemberOrder(sequence = "2")
+	public List<Cliente> listadoClienteActivos() {
+		List<Cliente> items = listaClientes();
+		if (items.isEmpty()) {
+			getContainer().informUser("No hay clientes activos :-(");
+		}
+		return items;
+	}
+	protected List<Cliente> listaClientes() {
+		return allMatches(Cliente.class, new Filter<Cliente>() {
+			@Override
+			public boolean accept(final Cliente t) {
+				return t.getActivo();
+			}
+		});
+	}
+	// }}
+	
+	// {{
+	@Hidden    
+	public List<Cliente> autoComplete(final String cliente) {
+		return allMatches(Cliente.class, new Filter<Cliente>() {
+		@Override
+		public boolean accept(final Cliente t) {		
+		return t.getNumeroIdent().contains(cliente) && t.getActivo(); 
+		}
+	  });				
+	}
+	// }}
+		 
+	// {{ Helpers
+	protected boolean ownedByCurrentUser(final Cliente t) {
+		return Objects.equal(t.getOwnedBy(), currentUserName());
+	}
+	protected String currentUserName() {
+		return getContainer().getUser().getName();
+	}
+	// }}
+}

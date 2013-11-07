617ee478d4c009fc11f45cc47196e0733f01e9ae
Matias Prado <matiasprado@msn.com>
2013-11-07 16:40:11 -0300

    Se agregó la entidad Adicional y su respectivo Servicio. Se incluyen carga y listado de los Adicionales de un Alquiler.

---
+++ dom/src/main/java/adicional/Adicional.java
@@ -0,0 +1,116 @@
+package adicional;
+
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.VersionStrategy;
+
+import org.apache.isis.applib.DomainObjectContainer;
+import org.apache.isis.applib.annotation.Audited;
+import org.apache.isis.applib.annotation.AutoComplete;
+import org.apache.isis.applib.annotation.Hidden;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.Named;
+import org.apache.isis.applib.annotation.NotPersisted;
+import org.apache.isis.applib.annotation.ObjectType;
+
+
+
+import alquiler.Alquiler;
+
+
+@javax.jdo.annotations.PersistenceCapable(identityType=IdentityType.DATASTORE)
+@javax.jdo.annotations.DatastoreIdentity(strategy=javax.jdo.annotations.IdGeneratorStrategy.IDENTITY)
+@javax.jdo.annotations.Version(strategy=VersionStrategy.VERSION_NUMBER, column="VERSION")
+@javax.jdo.annotations.Query(name="listaAdicionales", language="JDOQL",value="SELECT FROM adicional.Adicional where activo==true")
+@AutoComplete(repository=AdicionalServicio.class,action="autoComplete")
+@ObjectType("ADICIONAL")
+@Audited
+
+public class Adicional {
+	
+    // {{ Identification on the UI
+	@Named("Adicional")
+    public String title() {		
+        return getNombre().toString();
+	}     
+    // }}
+	
+	private String nombre;
+	@Hidden
+	//@Named("Adicional")
+	@MemberOrder(sequence="1")
+    public String getNombre(){
+    	return nombre;
+    }
+    public void setNombre(final String nombre){
+    	this.nombre=nombre;
+    }
+    
+    private String descripcion;
+    @Named("Descripcion")
+    @MemberOrder(sequence="2")
+    public String getDescripcion() {
+            return descripcion;
+    }
+    public void setDescripcion(final String descripcion) {
+            this.descripcion = descripcion;
+    }
+	
+    private float precio;
+    @Named("Precio x dia")
+    @MemberOrder(sequence="3")
+    public float getPrecio() {
+        return precio;
+    }
+    public void setPrecio(final float precio) {
+        this.precio = precio;
+    }
+    
+    private Alquiler alquiler;
+    @Hidden
+    @NotPersisted
+    @Named("Alquiler")
+    @MemberOrder(sequence="5")
+    public Alquiler getAlquiler() {
+            return alquiler;
+    }
+    public void setAlquiler(final Alquiler alquiler) {
+            this.alquiler = alquiler;
+    }
+    
+    // {{ Campo Activo
+   	private boolean activo;
+   	@Hidden
+   	@Named("Activo")
+   	@MemberOrder(sequence="6")
+   	public boolean getActivo() {
+   		return activo; 
+   	}   	
+   	public void setActivo(boolean activo){
+   		this.activo=activo; 
+   	}	
+    // }}   	   	
+   	//{{ Remove   	
+    @Named("Borrar")
+   	public void remove(){
+   		setActivo(false);
+   	}   	
+   	//}}  
+    
+	private String ownedBy;
+	@Hidden 
+	public String getOwnedBy() {
+	    return ownedBy;	
+	}
+	public void setOwnedBy(final String ownedBy){
+	    this.ownedBy = ownedBy;	
+	}	
+	// }}
+    
+    // {{ injected: DomainObjectContainer
+    @SuppressWarnings("unused")
+	private DomainObjectContainer container;
+
+    public void injectDomainObjectContainer(final DomainObjectContainer container) {
+       this.container = container;
+    }   
+}
---
+++ dom/src/main/java/adicional/AdicionalServicio.java
@@ -0,0 +1,81 @@
+package adicional;
+
+import java.util.List;
+
+
+import org.apache.isis.applib.AbstractFactoryAndRepository;
+import org.apache.isis.applib.annotation.Hidden;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.MultiLine;
+import org.apache.isis.applib.annotation.Named;
+import org.apache.isis.applib.annotation.Optional;
+import org.apache.isis.applib.filter.Filter;
+import org.apache.isis.applib.query.QueryDefault;
+
+import autos.Auto;
+
+import com.google.common.base.Objects;
+
+
+@Named("Adicionales")
+public class AdicionalServicio extends AbstractFactoryAndRepository{
+			
+	@MemberOrder(sequence = "1") 
+	@Named("Cargar Adicional")
+	public Adicional cargar(			
+			@Named("Nombre") String nombre,
+			@Optional
+			@MultiLine
+			@Named("Descripcion") String descrip,
+			@Named("Precio") float precio){
+			final boolean activo=true;
+			final String ownedBy = currentUserName();
+			return adicional(nombre,descrip,precio,activo,ownedBy);
+	}
+	@Hidden
+	public Adicional adicional(
+			final String nombre,
+			final String descrip,
+			final float precio,
+			final boolean activo,
+			final String userName) {
+		
+			Adicional adic=newTransientInstance(Adicional.class);
+			adic.setNombre(nombre);
+			adic.setDescripcion(descrip);
+			adic.setPrecio(precio);
+			adic.setActivo(activo);
+			adic.setOwnedBy(userName);
+			
+			persistIfNotAlready(adic);
+			
+			return adic;
+	}
+	
+    // {{ 
+    public List<Adicional> listaAdicionales() {         
+        return allMatches(QueryDefault.create(Adicional.class, "listaAdicionales"));
+    } 
+    // }}
+	
+	// {{  
+	@Hidden    
+	public List<Adicional> autoComplete(final String nombre) {
+		return allMatches(Adicional.class, new Filter<Adicional>() {
+		@Override
+		public boolean accept(final Adicional t) {		
+			return t.getNombre().contains(nombre) && t.getActivo();
+		}
+	  });				
+	}
+	// }}	
+	
+	// {{ Helpers
+	protected boolean ownedByCurrentUser(final Auto t) {
+	    return Objects.equal(t.getOwnedBy(), currentUserName());
+	}
+	protected String currentUserName() {
+	    return getContainer().getUser().getName();
+	}
+	// }}		
+}

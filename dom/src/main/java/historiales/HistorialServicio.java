0d885b68d5f4bd84bc3d980392d83bc6f266805c
silvana noemi figueroa <sil-figueroa@hotmail.com>
2013-11-15 02:56:54 -0300

    Se agrego el listado de historial de alquileres por cliente

--- dom/src/main/java/historiales/HistorialServicio.java
+++ dom/src/main/java/historiales/HistorialServicio.java
@@ -8,6 +8,8 @@ import org.apache.isis.applib.annotation.MemberOrder;
 import org.apache.isis.applib.annotation.Named;
 import org.apache.isis.applib.filter.Filter;
 
+import cliente.Cliente;
+
 import alquiler.Alquiler.EstadoAlquiler;
 import autos.Auto;
 import disponibles.AutoPorFecha;
@@ -15,6 +17,7 @@ import disponibles.AutoPorFecha;
 @Named("Historial")
 public class HistorialServicio extends AbstractFactoryAndRepository {
 	//Historial de autos alquilados
+	//{{
 	@MemberOrder(sequence = "1")
 	 public List<AutoPorFecha> historialPorAuto(final Auto patente) 
 	 { return allMatches(AutoPorFecha.class, new Filter<AutoPorFecha>(){ 
@@ -25,4 +28,15 @@ public class HistorialServicio extends AbstractFactoryAndRepository {
 		 }
 		});
 	 }
+	//}}
+	//{{Historial de alquileres por cliente
+	public List<AutoPorFecha> historialPorCliente(final Cliente cliente) 
+	 { return allMatches(AutoPorFecha.class, new Filter<AutoPorFecha>(){ 
+		 @Override 
+		 public boolean accept(final AutoPorFecha auto) 
+		 {  return auto.getAlquiler().getClienteId().equals(cliente) && auto.getAlquiler().getEstado()==EstadoAlquiler.FINALIZADO;  
+		 }
+		});
+	 }
+	//}}
 }

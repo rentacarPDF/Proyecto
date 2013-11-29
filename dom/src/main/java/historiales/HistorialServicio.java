361aefdd172c2f1d9f63e706a8fe74541598577a
Carla Pappalardo <carla.pappalardo@hotmail.com>
2013-11-29 18:18:08 -0300

    Se documenta el paquete Historiales
    Se documentan las Clases del paquete Historiales, con la herramienta JavaDoc

--- dom/src/main/java/historiales/HistorialServicio.java
+++ dom/src/main/java/historiales/HistorialServicio.java
@@ -1,39 +1,52 @@
 package historiales;
 
 import java.util.List;
-
-
 import org.apache.isis.applib.AbstractFactoryAndRepository;
 import org.apache.isis.applib.annotation.MemberOrder;
 import org.apache.isis.applib.annotation.Named;
 import org.apache.isis.applib.filter.Filter;
-
 import categoria.Categoria;
 import cliente.Cliente;
-
 import alquiler.Alquiler.EstadoAlquiler;
 import autos.Auto;
 import disponibles.AutoPorFecha;
 
+/**
+ * Clase que representa un Servicio de Historiales de Alquileres con distintos filtros.
+ *
+ */
 @Named("Historial")
 public class HistorialServicio extends AbstractFactoryAndRepository {
+	/**
+	 * Identificacion del nombre del icono que aparecera en la UI
+	 * @return String
+	 */
 	public String iconName(){
 		return "historial";
 	}
-	//Historial de autos alquilados
-	//{{
+	/**
+	 * Se retorna un historial de alquileres de un vehiculo en especifico.
+	 * 
+	 * @param patente
+	 * 
+	 * @return List<AutoPorFecha>
+	 */
 	@MemberOrder(sequence = "1")
 	 public List<AutoPorFecha> historialPorAuto(final Auto patente) 
-	 { return allMatches(AutoPorFecha.class, new Filter<AutoPorFecha>(){ 
+	 { 
+		return allMatches(AutoPorFecha.class, new Filter<AutoPorFecha>(){ 
 		 @Override 
 		 public boolean accept(final AutoPorFecha auto) 
-		 { 
-			 return auto.getPatente().contains(patente.getPatente()) && auto.getAlquiler().getEstado()==EstadoAlquiler.CERRADO; 
+		 { return auto.getPatente().contains(patente.getPatente()) && auto.getAlquiler().getEstado()==EstadoAlquiler.CERRADO; 
 		 }
 		});
 	 }
-	//}}
-	//{{Historial de alquileres por cliente
+	/**
+	 * 
+	 * Se retorna un historial de vehiculos alquilados por un Cliente en especifico.
+	 * @param cliente
+	 * @return List<AutoPorFecha>
+	 */
 	public List<AutoPorFecha> historialPorCliente(final Cliente cliente) 
 	 { return allMatches(AutoPorFecha.class, new Filter<AutoPorFecha>(){ 
 		 @Override 
@@ -42,7 +55,11 @@ public class HistorialServicio extends AbstractFactoryAndRepository {
 		 }
 		});
 	 }
-	//}}
+	/**
+	 * Se retorna un historial de vehiculos alquilados por una Categoria en especifica.
+	 * @param categoria
+	 * @return List<AutoPorFecha>
+	 */
 	public List<AutoPorFecha> historialPorCategoria(final Categoria categoria) 
 	 { return allMatches(AutoPorFecha.class, new Filter<AutoPorFecha>(){ 
 		 @Override 
